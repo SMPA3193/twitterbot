789b6e2b74e410766ca6b932b93f81c7c6a07374
Derek Willis <dwillis@gmail.com>
2016-04-20 08:24:32 -0400

    creds

--- tweetbot2.py
+++ tweetbot2.py
@@ -1,10 +1,10 @@
 from twython import Twython
 import csv
 
-APP_KEY = 'uqdFqqqnBq4ruOaGR1e2P2M9D'
-APP_SECRET = 'LpIvtuF9Gjb3EwMObQ22VOUVgj1HiZi7n5caVeZ3wJ98hDSqOQ'
-OAUTH_TOKEN = '722556363721818113-jGlE6zQLKMAn4DJqtz6MOyC7CCXODNB'
-OAUTH_TOKEN_SECRET = 'vAnhXdWepDcB2XHQYjzViarCMbXFM0pqPSPqfQqgk8Kpq'
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
 
 t = Twython(app_key=APP_KEY, app_secret=APP_SECRET, oauth_token=OAUTH_TOKEN, oauth_token_secret=OAUTH_TOKEN_SECRET)
 
