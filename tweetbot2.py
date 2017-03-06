f9661f2a247690c204a55978d1ad60863b257ad4
Derek Willis <dwillis@gmail.com>
2017-03-06 14:29:31 -0500

    Update tweetbot2.py

--- tweetbot2.py
+++ tweetbot2.py
@@ -8,17 +8,13 @@ ACCESS_TOKEN_SECRET = ''
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-search = twitter.search(q='propublica', count="100")
+search = twitter.search(q='YOUR SEARCH TERM HERE', count="100")
 tweets = search['statuses']
 
 with open ('data.csv', 'w') as fp:
     a = csv.writer(fp)
-
-#Open the CSV file. 'data.csv' can be renamed to whatever you would like. This is the filename it will save under.
-
+    # add a header row
     a.writerow(('Search Term', 'Tweet Text', 'URL'))
-
-#At the top of the CSV file, we want to add in a row with columns labeled 'Search Term' and 'Tweet Text'.
 
     for result in tweets:
         try:
